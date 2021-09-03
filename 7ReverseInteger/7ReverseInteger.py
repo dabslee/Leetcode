5693db2a5bae1bd174836117dba9390afd30b738
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-09-03 13:35:55 -0500

    optimized p7 slightly

--- 7ReverseInteger/7ReverseInteger.py
+++ 7ReverseInteger/7ReverseInteger.py
@@ -1,7 +1,9 @@
+# Complexity O(lg n)
+
 class Solution:
     def reverse(self, x: int) -> int:
-        ans = int(str(x)[::-1].rstrip("-"))
-        ans *= -1 if str(x)[0] == '-' else 1
+        ans = int(str(x).lstrip("-")[::-1])
+        ans *= -1 if x < 0 else 1
         if ans > 2**31 - 1 or ans < -2**31:
             return 0
         return ans
